3f3da0410cba05eed37c43a70907f0120f84622c
magnoparrot <magnoparrot33@gmail.com>
2023-04-23 19:11:18 -0300

    first task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Header from "./components/layout/Header";
+
+import Footer from "./components/layout/Footer";
+import Body from "./components/layout/Nav";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Body/>
+      <Footer/>
+      <BrowserRouter>
+      <Routes>
+      <Route path="Body" element={<Body/>} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/layout/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const Header = () => {
+  return (
+    
+    <header class="mi-header">
+    <nav>
+      <ul>
+        <li><a href="#inicio">Home</a></li>
+        <li><a href="#acerca-de">About US</a></li>
+        <li><a href="#servicios">Services</a></li>
+        <li><a href="#contacto">Contact Us</a></li>
+      </ul>
+    </nav>
+  </header>
+  );
+}
+
+export default Header;
